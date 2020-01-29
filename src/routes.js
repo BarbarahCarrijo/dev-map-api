a9ed76efc3482a18d7ce96c724636fc835a8404d
Barbara Carrijo <barbarahcarrijo@gmail.com>
2020-01-29 14:13:20 -0300

    Refactor startup files, add db connection settings and update gitignore

---
+++ src/routes.js
@@ -0,0 +1,29 @@
+const {Router} = require('express');
+const axios = require('axios');
+const Dev = require('./models/Dev');
+
+const routes = Router();
+
+routes.post('/devs', async (req, res) => {
+    const {github_username, techs} = req.body;
+
+    const apiRes = await axios.get(`https://api.github.com/users/${github_username}`);
+
+    const {name = login, avatar_url, bio} = apiRes.data;
+
+    const techsArray = techs.split(',').map(tech => tech.trim());
+
+    const dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray
+    });
+
+    //console.log(name, avatar_url, bio, github_username);
+
+    return res.json({dev});
+});
+
+module.exports = routes;
