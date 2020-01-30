b17b0f628b9ea4205dd0df02faef75e6326db405
Barbara Carrijo <barbarahcarrijo@gmail.com>
2020-01-30 00:04:07 -0300

    Add controllers and configure routes

--- src/routes.js
+++ src/routes.js
@@ -1,35 +1,13 @@
 const {Router} = require('express');
-const axios = require('axios');
-const Dev = require('./models/Dev');
+const DevController = require('./controllers/DevController');
+const SearchController = require('./controllers/SearchController');
 
 const routes = Router();
 
-routes.post('/devs', async (req, res) => {
-    const {github_username, techs, latitude, longitude} = req.body;
+routes.get('/devs', DevController.index);
+routes.post('/devs', DevController.store);
 
-    const apiRes = await axios.get(`https://api.github.com/users/${github_username}`);
+routes.get('/search', SearchController.index);
 
-    const {name = login, avatar_url, bio} = apiRes.data;
-
-    const techsArray = techs.split(',').map(tech => tech.trim());
-
-    const location = {
-        type: 'Point',
-        coordinates: [longitude, latitude]
-    };
-
-    const dev = await Dev.create({
-        github_username,
-        name,
-        avatar_url,
-        bio,
-        techs: techsArray,
-        location
-    });
-
-    //console.log(name, avatar_url, bio, github_username);
-
-    return res.json({dev});
-});
 
 module.exports = routes;
